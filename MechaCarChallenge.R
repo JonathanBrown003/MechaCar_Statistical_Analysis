f77d8ef4614efbd80107cfd92ea22c6424474cae
JonathanBrown003 <jdbrown00003@gmail.com>
2021-12-21 18:07:18 -0600

    Adding Deliverable 3 Files

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -17,3 +17,17 @@ total_summary <- sus_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), 
 
 # Create a summary data frame by Manufacturing Lot
 lot_summary <- sus_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI),Variance = var(PSI), SD = sd(PSI))
+
+# Determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch
+t.test(sus_coil$PSI, mu = 1500)
+
+# Write three more RScripts in your MechaCarChallenge.RScript using the t.test() function and its subset() argument to determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+Lot1 = subset(sus_coil, Manufacturing_Lot == 'Lot1')
+Lot2 = subset(sus_coil, Manufacturing_Lot == 'Lot2')
+Lot3 = subset(sus_coil, Manufacturing_Lot == 'Lot3')
+
+t.test(Lot1$PSI, mu = 1500)
+
+t.test(Lot2$PSI, mu = 1500)
+
+t.test(Lot3$PSI, mu = 1500)
